021b1946711bc76b1a1608a435147a92f10de321
Yaroslav <yaroslav.kovalev.2005@gmail.com>
2025-12-17 05:00:46 +1000

    created basic structure of project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int _mineralAmount;
+
+    [SerializeField] private byte _kingdomLevel;
+
+    [SerializeField] private int _gold;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
