8b2310ec5b092e6438a740ceab1b8b17dbeaef36
Yaroslav <yaroslav.kovalev.2005@gmail.com>
2025-12-17 23:39:15 +1000

    development of the kingdom area has begun

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;      // Объект игрока
+    public float smoothSpeed = 0.125f; // Скорость сглаживания
+    public Vector3 offset;       // Смещение (обычно по Z: -10)
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        // Желаемая позиция камеры
+        Vector3 desiredPosition = target.position + offset;
+
+        // Плавный переход от текущей позиции к желаемой
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+    public float speed = 10;
+    private Vector2 moveVector;
+    private SpriteRenderer sr;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
-    {
+    {        
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.y = Input.GetAxis("Vertical");
+        if (moveVector.x > 0)
+        {
+            sr.flipX = true;
+        }
+        else if (moveVector.x < 0)
+        {
+            sr.flipX = false;
+        }
+        rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
         
     }
 }
